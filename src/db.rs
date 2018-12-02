8ba74b792d289f08c0b5d085c23634e5e1bcfdb8
colinbankier <colinbankier@gmail.com>
2018-12-02 22:40:54 +1000

    Add fix for passing closure to 'blocking'.
    Allows 'Repo' to handle diesel calls async.

--- src/db.rs
+++ src/db.rs
@@ -8,14 +8,14 @@ use tokio_threadpool::blocking;
 pub type ConnectionPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 pub type Connection = r2d2::PooledConnection<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
 
+/// A database "repository", for running database workloads.
+/// Manages a connection pool and running blocking tasks in a
+/// way that does not block the tokio event loop.
 #[derive(Clone)]
 pub struct Repo {
     connection_pool: ConnectionPool,
 }
 
-/// A database "repository", for running database workloads.
-/// Manages a connection pool and running blocking tasks in a
-/// way that does not block the tokio event loop.
 impl Repo {
     pub fn new() -> Self {
         Repo {
@@ -24,7 +24,7 @@ impl Repo {
     }
 
     /// Runs the given closure in a way that is safe for blocking IO to the database.
-    /// The closure will passed a `Connection` from the pool to use.
+    /// The closure will be passed a `Connection` from the pool to use.
     pub async fn run<F, T>(&self, f: F) -> Result<T, tokio::io::Error>
     where
         F: FnOnce(Connection) -> T + Send + std::marker::Unpin + 'static,
@@ -34,16 +34,13 @@ impl Repo {
         // `tokio_threadpool::blocking` returns a `Poll` compatible with "old style" futures.
         // `poll_fn` converts this into a future, then
         // `tokio::await` is used to convert the old style future to a `std::futures::Future`.
-        //
-        // Currently fails compilation with:
-        // |     pub async fn run<F, T>(&self, f: F) -> Result<T, tokio::io::Error>
-        // |                                   - captured outer variable
-        //
-        // |             || blocking(|| f(pool.get().unwrap())).map_err(|_| panic!("the threadpool shut down"))
-        // |                         ^^^^^^^^^^^^^^^^^^^^^^^^^ cannot move out of captured variable in an `FnMut` closure
-        tokio::await!(poll_fn(
-            || blocking(|| f(pool.get().unwrap())).map_err(|_| panic!("the threadpool shut down"))
+        // `f.take()` allows the borrow checker to be sure `f` is not moved into the inner closure
+        // multiple times if `poll_fn` is called multple times.
+        let mut f = Some(f);
+        tokio::await!(poll_fn(|| blocking(|| (f.take().unwrap())(
+            pool.get().unwrap()
         ))
+        .map_err(|_| panic!("the threadpool shut down"))))
     }
 }
 
