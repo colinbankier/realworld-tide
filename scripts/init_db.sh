fda1018720baf42b1a586f24b8cd62ab632c365e
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-09-14 11:40:21 +0100

    Use a single script to init the db

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -38,6 +38,7 @@ else
     DB_PORT="${POSTGRES_PORT}"
 fi
 
+# Launch postgres using Docker
 docker run \
     --name realworld-postgres --rm \
     -e POSTGRES_USER=${DB_USER} \
@@ -45,3 +46,16 @@ docker run \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres
+
+# Keep pinging Postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running - ready to be used!"
+
+>&2 echo "Running migrations."
+# Run migrations
+diesel database setup \
+    --database-url=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
