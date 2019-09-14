2218700dfc8264f8ac3813edf05cf76bc22f22ef
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-09-14 12:21:46 +0100

    Avoid using cargo make

---
+++ scripts/run_tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+# Check if a user has been set, otherwise default to 'postgres'
+DB_USER=${POSTGRES_USER:=postgres}
+# Check if a password has been set, otherwise default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# Check if a password has been set, otherwise default to 'realworld'
+DB_NAME="${POSTGRES_DB:=realword}"
+# Check if a port has been set, otherwise default to '5432'
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+
+# Reset DB (in case it was left in a corrupted state)
+diesel database reset \
+    --database-url=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+
+# Run tests
+cargo test
+
+# Reset DB
+diesel database reset \
+    --database-url=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
