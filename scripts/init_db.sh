c00836349217839470bc1e91ec3d42f268340c5c
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-09-14 12:02:33 +0100

    Simplify script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,41 +2,14 @@
 set -x
 set -eo pipefail
 
-# Check if a user has been set
-if [[ -z "${POSTGRES_USER}" ]]
-then
-    echo "POSTGRES_USER has not been set, defaulting to 'postgres'"
-    DB_USER=postgres
-else
-    DB_USER="${POSTGRES_USER}"
-fi
-
-# Check if a password has been set
-if [[ -z "${POSTGRES_PASSWORD}" ]]
-then
-    echo "POSTGRES_PASSWORD has not been set, defaulting to 'password'"
-    DB_PASSWORD=password
-else
-    DB_PASSWORD="${POSTGRES_PASSWORD}"
-fi
-
-# Check if a database name has been set
-if [[ -z "${POSTGRES_DB}" ]]
-then
-    echo "POSTGRES_DB has not been set, defaulting to 'diesel_demo'"
-    DB_NAME=diesel_demo
-else
-    DB_NAME="${POSTGRES_DB}"
-fi
-
-# Check if a port has been set
-if [[ -z "${POSTGRES_PORT}" ]]
-then
-    echo "POSTGRES_PORT has not been set, defaulting to '5432'"
-    DB_PORT=5432
-else
-    DB_PORT="${POSTGRES_PORT}"
-fi
+# Check if a user has been set, otherwise default to 'postgres'
+DB_USER=${POSTGRES_USER:=postgres}
+# Check if a password has been set, otherwise default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# Check if a password has been set, otherwise default to 'realworld'
+DB_NAME="${POSTGRES_DB:=realword}"
+# Check if a port has been set, otherwise default to '5432'
+DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Launch postgres using Docker
 docker run \
