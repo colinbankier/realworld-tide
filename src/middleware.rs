91f5fbee24db28e4d13271d4f890f847d58f2988
Luca Palmieri <lpalmieri@truelayer.com>
2019-12-22 15:43:44 +0000

    Fix last compiler error

--- src/middleware.rs
+++ src/middleware.rs
@@ -27,16 +27,13 @@ impl<State> ContextExt for Request<State> {
 impl<State: Send + Sync + 'static> Middleware<State> for JwtMiddleware {
     fn handle<'a>(
         &'a self,
-        mut cx: Request<State>,
+        cx: Request<State>,
         next: Next<'a, State>,
     ) -> BoxFuture<'a, Response> {
         Box::pin(async move {
             let claims = extract_claims(cx.headers());
             if let Some(c) = claims {
-                // The `let _ = ...` is a workaround for issue: https://github.com/rustasync/tide/issues/278
-                // Solution is according to suggestion in https://github.com/rust-lang/rust/issues/61579#issuecomment-500436524
-                let _ = cx.set_local(c);
-                return next.run(cx).await;
+                return next.run(cx.set_local(c)).await;
             } else {
                 return Response::new(403);
             }
