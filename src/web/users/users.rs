5f9f8c6408b39dd3b19e8e32d10a7de0cd41734b
Luca Palmieri <lpalmieri@truelayer.com>
2019-12-25 19:53:45 +0000

    Return a response with the correct schema for updating user.

--- src/web/users/users.rs
+++ src/web/users/users.rs
@@ -6,6 +6,7 @@ use crate::Repo;
 use log::info;
 use serde::Deserialize;
 
+use crate::web::users::responses::UserResponse;
 use http::status::StatusCode;
 use tide::{Request, Response};
 
@@ -23,7 +24,10 @@ pub async fn update_user(mut cx: Request<Repo>) -> tide::Result<Response> {
     let results = users::update(repo, auth.user_id(), update_params.user);
 
     match results {
-        Ok(b) => Ok(Response::new(200).body_json(&b).unwrap()),
+        Ok(user) => {
+            let response = UserResponse { user };
+            Ok(Response::new(200).body_json(&response).unwrap())
+        }
         Err(e) => Err(diesel_error(&e)),
     }
 }
