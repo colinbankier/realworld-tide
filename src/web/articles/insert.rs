9d56e1cee919ef26d1391a2e8259fea9079f8411
Luca Palmieri <lpalmieri@truelayer.com>
2019-12-27 11:02:52 +0000

    Create a slug from the article title

--- src/web/articles/insert.rs
+++ src/web/articles/insert.rs
@@ -5,6 +5,7 @@ use crate::web::articles::responses::Article;
 use crate::web::diesel_error;
 use crate::Repo;
 use http::status::StatusCode;
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use tide::Response;
 
@@ -35,25 +36,29 @@ pub async fn insert_article(mut cx: tide::Request<Repo>) -> tide::Result<Respons
         .await
         .map_err(|e| Response::new(400).body_string(e.to_string()))?;
     let auth = cx.get_claims().map_err(|_| StatusCode::UNAUTHORIZED)?;
-
     let repo = cx.state();
+
     let user = users::find(repo, auth.user_id()).map_err(|e| diesel_error(&e))?;
     let new_article = NewArticle {
         description: new_article.article.description,
+        slug: get_slug(new_article.article.title.clone()),
         title: new_article.article.title,
         body: new_article.article.body,
-        slug: "".to_string(),
         tag_list: new_article.article.tag_list,
         user_id: user.id,
     };
-    let result = articles::insert(repo, new_article);
-    match result {
-        Ok(article) => {
-            let response = NewArticleResponse {
-                article: Article::new(article, user, 0),
-            };
-            Ok(Response::new(200).body_json(&response).unwrap())
-        }
-        Err(e) => Err(diesel_error(&e)),
-    }
+
+    let article = articles::insert(repo, new_article).map_err(|e| diesel_error(&e))?;
+    let response = NewArticleResponse {
+        article: Article::new(article, user, 0),
+    };
+    Ok(Response::new(200).body_json(&response).unwrap())
+}
+
+/// Convert a title into a url-safe slug
+fn get_slug(title: String) -> String {
+    title
+        .to_ascii_lowercase()
+        .split_ascii_whitespace()
+        .join("-")
 }
