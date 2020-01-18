06b0c14ebebd8da4dfd7c698b39a338a6e5f74cf
Luca Palmieri <lpalmieri@truelayer.com>
2020-01-18 08:49:06 +0000

    Remove unused methods

--- src/web/articles/responses.rs
+++ src/web/articles/responses.rs
@@ -1,4 +1,3 @@
-use crate::db::models;
 use crate::domain;
 use crate::domain::Profile;
 use chrono::{DateTime, Utc};
@@ -11,10 +10,44 @@ pub struct ArticlesResponse {
     pub articles_count: u64,
 }
 
+impl From<Vec<domain::ArticleView>> for ArticlesResponse {
+    fn from(articles: Vec<domain::ArticleView>) -> Self {
+        let articles_count = articles.len() as u64;
+        let articles = articles.into_iter().map(|a| Article::from(a)).collect();
+        Self {
+            articles,
+            articles_count,
+        }
+    }
+}
+
+impl From<Vec<domain::Article>> for ArticlesResponse {
+    fn from(articles: Vec<domain::Article>) -> Self {
+        let articles_count = articles.len() as u64;
+        let articles = articles.into_iter().map(|a| Article::from(a)).collect();
+        Self {
+            articles,
+            articles_count,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct ArticleResponse {
     pub article: Article,
+}
+
+impl From<domain::Article> for ArticleResponse {
+    fn from(a: domain::Article) -> Self {
+        Self { article: a.into() }
+    }
+}
+
+impl From<domain::ArticleView> for ArticleResponse {
+    fn from(a: domain::ArticleView) -> Self {
+        Self { article: a.into() }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -66,40 +99,6 @@ impl From<domain::ArticleView> for Article {
     }
 }
 
-impl From<domain::Article> for ArticleResponse {
-    fn from(a: domain::Article) -> Self {
-        Self { article: a.into() }
-    }
-}
-
-impl From<domain::ArticleView> for ArticleResponse {
-    fn from(a: domain::ArticleView) -> Self {
-        Self { article: a.into() }
-    }
-}
-
-impl Article {
-    pub fn new(a: models::Article, u: models::User, n_fav: u64, favorited: bool) -> Self {
-        Self {
-            title: a.title,
-            slug: a.slug,
-            description: a.description,
-            body: a.body,
-            favorited,
-            favorites_count: n_fav as u64,
-            created_at: a.created_at,
-            updated_at: a.updated_at,
-            tag_list: a.tag_list,
-            author: Author {
-                username: u.username,
-                bio: u.bio,
-                image: u.image,
-                following: false,
-            },
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Author {
@@ -127,42 +126,6 @@ impl From<domain::ProfileView> for Author {
             bio: p.profile.bio,
             image: p.profile.image,
             following: p.following,
-        }
-    }
-}
-
-impl ArticlesResponse {
-    pub fn new(results: Vec<(models::Article, models::User, u64, bool)>) -> Self {
-        let articles_count = results.len() as u64;
-        let articles = results
-            .into_iter()
-            .map(|(a, u, n_fav, favorited)| Article::new(a, u, n_fav, favorited))
-            .collect();
-        Self {
-            articles,
-            articles_count,
-        }
-    }
-}
-
-impl From<Vec<domain::ArticleView>> for ArticlesResponse {
-    fn from(articles: Vec<domain::ArticleView>) -> Self {
-        let articles_count = articles.len() as u64;
-        let articles = articles.into_iter().map(|a| Article::from(a)).collect();
-        Self {
-            articles,
-            articles_count,
-        }
-    }
-}
-
-impl From<Vec<domain::Article>> for ArticlesResponse {
-    fn from(articles: Vec<domain::Article>) -> Self {
-        let articles_count = articles.len() as u64;
-        let articles = articles.into_iter().map(|a| Article::from(a)).collect();
-        Self {
-            articles,
-            articles_count,
         }
     }
 }
